7d5967a061628198cc4035c869d7370bda9a7721
Chris <surni@gmx.at>
2025-03-08 22:23:53 +0100

    added a print function

--- app.c
+++ app.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* create_month(int n_days) {
+    int* c = (int*) malloc(n_days * sizeof(int));
+    for(int i=0; i < n_days; i++) {
+        c[i] = i + 1;
+    }
+    return c;
+}
+
+void print_month(int* month) {
+    for(int i=0; i < 31 ; i++) {
+        if(i % 7 == 0)
+            printf("\n");
+        if(i + 1 < 10)
+            printf("%d  ", month[i]);
+        else
+            printf("%d ", month[i]);
+    }
+    printf("\n");
+}
 
 
 void main(void) {
-    char *calender = NULL;
+    int *calender = NULL;
     int month;
     for(;;) {
         printf("%s\n", "Enter a month: 1 to 12 or 0 to quit");
@@ -11,10 +32,12 @@ void main(void) {
         switch (month)
         {
             case 0:
-                printf("\n Good Bye!");
+                printf("\nGood Bye!");
                 goto end;
             case 1:
                 printf("%s", "Jan.\n");
+                int* m = create_month(31);
+                print_month(m);
                 break;
             
             default:
