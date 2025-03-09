96142f1d176b6db5d9feea0b45daa55a7806609f
Chris <surni@gmx.at>
2025-03-09 11:22:08 +0100

    new structered program

--- app.c
+++ app.c
@@ -1,45 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* create_month(int n_days) {
-    int* c = (int*) malloc(n_days * sizeof(int));
-    for(int i=0; i < n_days; i++) {
-        c[i] = i + 1;
-    }
-    return c;
+struct month {
+    char *name;
+    int n_days;
+    void (*print)(int);
+};
+
+
+typedef struct month Month;
+const char *names[] = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dez"};
+const int n_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+void print_month(int n_days) {
+    printf("\n%d\n", n_days);
 }
 
-void print_month(int* month) {
-    for(int i=0; i < 31 ; i++) {
-        if(i % 7 == 0)
-            printf("\n");
-        if(i + 1 < 10)
-            printf("%d  ", month[i]);
-        else
-            printf("%d ", month[i]);
+void init_calender(Month cal[]) {
+    for(int i=0; i < 12; i++) {
+        Month m = {.name = names[i] + '\0', .n_days = n_days[i], .print = print_month};
+        cal[i] = m; 
     }
-    printf("\n");
 }
+
+void print(Month cal[], int month) {
+    cal[month].print(cal[month].n_days);
+}
+
 
 
 void main(void) {
-    int *calender = NULL;
-    int month;
+    const Month calender[12];
+    init_calender(calender);
+    
+    int val;
     for(;;) {
         printf("%s\n", "Enter a month: 1 to 12 or 0 to quit");
-        scanf("%d", &month);
+        scanf("%d", &val);
 
-        switch (month)
+        switch (val)
         {
             case 0:
-                printf("\nGood Bye!");
                 goto end;
             case 1:
-                printf("%s", "Jan.\n");
-                int* m = create_month(31);
-                print_month(m);
+                print(calender, val-1);
                 break;
-            
+            case 2:
+                print(calender, val-1);
+                break;
+            case 3:
+                print(calender, val-1);
+                break;
+            case 4:
+                print(calender, val-1);
+                break;
             default:
                 printf("Something went wrong. Check you input next time. Range: 1-12\n");
         }
